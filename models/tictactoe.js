5c418711946c1f12b1a36b7425f107f8f3ff9268
MiacAlexCoon <parfentjev.been@gmail.com>
2014-06-24 11:39:00 +0400

    Some fixes

---
+++ models/tictactoe.js
@@ -0,0 +1,115 @@
+var util = require('util'), 
+    EventEmitter = require('events').EventEmitter;
+
+var TicTacToe = module.exports = function() {
+    // Инициализируем события
+    EventEmitter.call(this);
+    // Массив id игры = объект игры
+    this.games = [];
+    // Массив подключённых пользователей = id игры
+    this.users = [];
+    // Массив пользователей ожидающих оппонентов для начало игры
+    this.free = [];
+}
+util.inherits(TicTacToe, EventEmitter);
+
+var GameItem = function(user, opponent) {
+    // Инициализируем события
+    EventEmitter.call(this);
+    // Ячейки игрового поля
+    this.board = [];
+    // Игроки
+    this.user = user; // X
+    this.opponent = opponent; // O
+    // Кол-во сделанных ходов
+    this.steps = 0;
+    // Кто ходит
+    this.turn = 'X';
+}
+util.inherits(GameItem, EventEmitter);
+
+/**
+ * Сделан ход
+ */
+GameItem.prototype.step = function(x, y, user, cb) {
+    if(this.board[x + '_' + y] !== undefined || this.getTurn(user) != this.turn) return;
+    this.board[x + '_' + y] = this.getTurn(user);
+    this.turn = (user != this.user ? 'X' : 'O');
+    this.steps++;
+    cb(this.checkWinner(x, y, this.getTurn(user)), this.getTurn(user));
+}
+
+TicTacToe.prototype.step = function(gameId, x, y, user, cb) {
+    console.info('Step');
+    console.dir(this.games[gameId]);
+    this.games[gameId].step(x, y, user, cb);
+}
+
+/**
+ * Запускаем игру
+ */
+TicTacToe.prototype.start = function(user, cb) {
+    // Размер игрового поля и кол-во ходов для победы
+    // Ищем свободные игры
+    if(Object.keys(this.free).length > 0) {
+        var opponent = Object.keys(this.free).shift();
+        delete this.free[opponent];
+        // Если есть ожидающие игру, создаём им игру
+        var game = new GameItem(user, opponent);
+        var id = [
+            Math.random() * 0xffff | 0
+            , Math.random() * 0xffff | 0
+            , Math.random() * 0xffff | 0
+            , Date.now()
+        ].join('-');
+        // Добавляем игру в список действующих
+        this.games[id] = game;
+        this.users[user] = id;
+        this.users[opponent] = id;
+        console.dir(this.games[id]);
+        game.emit('timer', 'start', user);
+        cb(true, id, opponent);
+    } else {
+        // Пока нет, значит будем ждать
+        this.free[user] = true;
+        cb(false);
+    }
+}
+
+/**
+ * Получаем чем ходит игрок
+ */
+GameItem.prototype.getTurn = function(user) {
+    return (user == this.user ? 'X' : 'O');
+}
+
+/**
+ * Выходим из игры
+ */
+TicTacToe.prototype.end = function(user, cb) {
+    delete this.free[user];
+    if(this.users[user] === undefined) return;
+    var gameId = this.users[user];
+    if(this.games[gameId] === undefined) return;
+    var game = this.games[gameId];
+    var opponent = (user == game.user ? game.opponent : game.user);
+    var turn = game.turn;
+    delete this.games[gameId];
+    game = null;
+    delete this.users[user];
+    cb(gameId, opponent, turn);
+}
+
+/**
+ * Проверяем нет ли победителя
+ */
+GameItem.prototype.checkWinner = function(x, y, turn) {
+    if(false) {
+        // есть победитель
+        return true;
+    } else {
+        // нет победителя
+        return false;
+    }
+}
+
---
+++ public/javascripts/tictactoe.js
@@ -0,0 +1,119 @@
+var TicTacToe = {
+    gameId: null,
+    turn: null,
+    i: false,
+    interval: null,
+    init: function() {
+        $(function() {
+            // Подключаемся к серверу nodejs с socket.io
+            var socket = io.connect(window.location.hostname + ':3000');
+            $('#reload').hide().button({icons:{primary:'ui-icon-refresh'}}).click(function() {
+                $('#reload').off('click').click(function(){window.location.reload();});
+                socket.emit('start');
+            });
+            socket.on('connect', function () {
+                $('#status').html('Успешно подключились к игровому серверу');
+                $('#reload').show();
+                board.init(function (e) {
+                    console.log(e);
+                    var x;
+                    var y;
+                    if (e.pageX != undefined && e.pageY != undefined) {
+                      x = e.pageX/board.cellSize;
+                      y = e.pageY/board.cellSize;
+                      console.log("User clicked on: " + x + ", " + y);
+                      console.log(TicTacToe.i);
+                      if(TicTacToe.i) {
+                        socket.emit('step', TicTacToe.gameId, x + "_" + y);
+                      }
+                    }
+                });
+            });
+            socket.on('reconnect', function () {
+                $('#reload').show();
+                $('#connect-status').html('Переподключились, продолжайте игру');
+            });
+            socket.on('reconnecting', function () {
+                $('#reload').hide();
+                $('#status').html('Соединение с сервером потеряно, переподключаемся...');
+            });
+            socket.on('error', function (e) {
+                $('#status').html('Ошибка: ' + (e ? e : 'неизвестная ошибка'));
+            });
+            // Ожидаем соперника
+            socket.on('wait', function(){
+                $('#status').append('... Ожидаем соперника...');
+            });
+            // Соперник отлючился
+            socket.on('exit', function(){
+                TicTacToe.endGame(TicTacToe.turn, 'exit');
+            });
+            // Время на ход вышло
+            socket.on('timeout', function(turn) {
+                TicTacToe.endGame(turn, 'timeout');
+            });
+            // К нам подключился соперник, начинаем игру
+            socket.on('ready', function(gameId, turn, x, y) {
+                $('#status').html('К вам подключился соперник! Игра началась! ' 
+                    + (turn == 'X' ? 'Сейчас Ваш первый ход' : 'Сейчас ходит соперник') + '!');
+                TicTacToe.startGame(gameId, turn);
+            });
+            // Получаем ход
+            socket.on('step', function(id, turn, win) {
+                console.info('step', id, turn, win);
+                TicTacToe.move(id, turn, win);
+            });
+            // Статистика
+            socket.on('stats', function (arr) {
+                console.log(arr);
+            });
+        });
+    },
+
+    startGame: function (gameId, turn, x, y) {
+        this.gameId = gameId;
+        this.turn = turn;
+        this.i = (turn == 'X');
+        //initial UI
+        this.mask(!this.i);
+    },
+
+    mask: function(state) {
+        console.log(state);
+    },
+
+    move: function (id, turn, win) {
+        this.i = (turn != this.turn);
+        $("#" + id).attr('class', 'ui-state-hover').html(turn);
+        if (!win) {
+            this.mask(!this.i);
+            $('#status').html('Сейчас ' + (this.i ? 'ваш ход' : 'ходит соперник'));
+        } else {
+            this.endGame(turn, win);
+        }
+    },
+
+    endGame: function (turn, win) {
+        clearInterval(this.interval);
+        var text = '';
+        switch(win) {
+            case 'none': text = 'Ничья!'; break;
+            case 'timeout': text = (turn == this.turn ? 'Слишком долго думали! Вы проиграли!' : 'Соперник так и не смог решить как ему ходить! Вы победили!'); break;
+            case 'exit': text = 'Соперник сбежал с поля боя! Игра закончена'; break;
+            default: text = 'Вы ' + (this.i ? 'проиграли! =(' : 'выиграли! =)');
+        }
+        $("<div/>").html(text).dialog({
+            title: 'Конец игры',
+            modal: true,
+            closeOnEscape: false,
+            resizable: false,
+            buttons: { "Играть снова": function() {
+                $(this).dialog("close");
+                window.location.reload();
+            }},
+            close: function() {
+                window.location.reload();
+            }
+        });
+    }
+};
